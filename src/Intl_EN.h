2ab1fdea4c6eef539052c38c228241810c2b4dae
DevChew <the@devchew.com>
2024-11-01 17:39:34 +0100

    Refactor internationalization to support multiple languages

---
+++ src/Intl_EN.h
@@ -0,0 +1,30 @@
+#ifndef INTL_EN
+#define INTL_EN
+
+// ENGLISH
+#define TEXT_MENU "menu"
+#define TEXT_NEXT "next"
+#define TEXT_EXIT "exit"
+#define TEXT_SAVE "save"
+#define TEXT_OK "ok"
+
+#define TEXT_YES "yes"
+#define TEXT_NO "no"
+
+#define TEXT_ON "on"
+#define TEXT_OFF "off"
+
+#define TEXT_MANUAL "manual"
+#define TEXT_AUTO "auto"
+
+#define TEXT_PUMP "pump"
+#define TEXT_MODE "mode"
+
+#define TEXT_WINDOW "window"
+#define TEXT_DURATION "duration"
+
+#define TEXT_TIME "time"
+#define TEXT_SCHEDULE "schedule"
+#define TEXT_INTERVAL "interval"
+
+#endif
